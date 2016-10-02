1f3d4daf59f25b77c6d3c322aa6ee336966cf59a
Ayush Sinha <ayushsinha1995@gmail.com>
2016-10-02 20:24:14 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+colleges = ["COEP","AISSMS","CUMMINS","WADIA"]
+print(colleges[2])
+if len(colleges)>3:
+          print("First college is", colleges[0])
+          print("First college is", colleges[1])
+          print("First college is", colleges[2])
+          print("First college is", colleges[3])
+print(colleges[2]+colleges[3])
+university = ["Pune","Mumbai"]
+print(colleges+university)
+print(colleges[1:3])
